560fbf7281961a16b2d1494158368c11f6418ea9
Poornaiah-4236 <168422073+Poornaiah-4236@users.noreply.github.com>
2024-04-29 16:14:41 +0530

    Add files via upload

---
+++ BDDUpdateProject_TestNG/src/test/java/com/bdd/DriverUtilites/WebDriver.java
@@ -0,0 +1,8 @@
+package com.bdd.DriverUtilites;
+
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class WebDriver {
+	public static String driverPath = "";
+	public RemoteWebDriver driver = null;
+}
